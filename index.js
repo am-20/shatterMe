d174f4279297bc0db40486d4b0404789a0dd01f6
am-20 <55308019+am-20@users.noreply.github.com>
2020-12-24 20:52:25 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+const names = document.getElementsByClassName("name");
+const quotes = document.getElementsByClassName("quote");
+
+for (var i = 0; i < quotes.length; i++) {
+  switch (i) {
+    case 0:
+      quotes[0].addEventListener("click", function () {
+        quotes[0].classList.toggle("appear");
+        names[0].classList.toggle("disappear");
+      });
+      break;
+    case 1:
+      quotes[1].addEventListener("click", function () {
+        quotes[1].classList.toggle("appear");
+        names[1].classList.toggle("disappear");
+      });
+      break;
+    case 2:
+      quotes[2].addEventListener("click", function () {
+        quotes[2].classList.toggle("appear");
+        names[2].classList.toggle("disappear");
+      });
+      break;
+    case 3:
+      quotes[3].addEventListener("click", function () {
+        quotes[3].classList.toggle("appear");
+        names[3].classList.toggle("disappear");
+      });
+      break;
+    case 4:
+      quotes[4].addEventListener("click", function () {
+        quotes[4].classList.toggle("appear");
+        names[4].classList.toggle("disappear");
+      });
+      break;
+    case 5:
+      quotes[5].addEventListener("click", function () {
+        quotes[5].classList.toggle("appear");
+        names[5].classList.toggle("disappear");
+      });
+      break;
+    case 6:
+      quotes[6].addEventListener("click", function () {
+        quotes[6].classList.toggle("appear");
+        names[6].classList.toggle("disappear");
+      });
+      break;
+    case 7:
+      quotes[7].addEventListener("click", function () {
+        quotes[7].classList.toggle("appear");
+        names[7].classList.toggle("disappear");
+      });
+      break;
+    case 8:
+      quotes[8].addEventListener("click", function () {
+        quotes[8].classList.toggle("appear");
+        names[8].classList.toggle("disappear");
+      });
+      break;
+    case 9:
+      quotes[9].addEventListener("click", function () {
+        quotes[9].classList.toggle("appear");
+        names[9].classList.toggle("disappear");
+      });
+      break;
+    case 10:
+      quotes[10].addEventListener("click", function () {
+        quotes[10].classList.toggle("appear");
+        names[10].classList.toggle("disappear");
+      });
+      break;
+    case 11:
+      quotes[11].addEventListener("click", function () {
+        quotes[11].classList.toggle("appear");
+        names[11].classList.toggle("disappear");
+      });
+      break;
+    case 12:
+      quotes[12].addEventListener("click", function () {
+        quotes[12].classList.toggle("appear");
+        names[12].classList.toggle("disappear");
+      });
+      break;
+    case 13:
+      quotes[13].addEventListener("click", function () {
+        quotes[13].classList.toggle("appear");
+        names[13].classList.toggle("disappear");
+      });
+      break;
+    case 14:
+      quotes[14].addEventListener("click", function () {
+        quotes[14].classList.toggle("appear");
+        names[14].classList.toggle("disappear");
+      });
+      break;
+  }
+}
